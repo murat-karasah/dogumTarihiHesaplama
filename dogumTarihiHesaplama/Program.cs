0fc98f30ed2d66a84f18e591126829235937fee5
Murat Karasah <mkarasah@gmail.com>
2021-11-19 12:06:20 +0300

    Add project files.

---
+++ dogumTarihiHesaplama/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace dogumTarihiHesaplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime tarih = DateTime.Now;
+            Console.WriteLine("Doğum Yılınızı Giriniz");
+            int yıl = tarih.Year;
+            int dogumtarihi = Convert.ToInt32(Console.ReadLine());
+
+            int sonuc = yıl - dogumtarihi;
+
+            Console.WriteLine("Yaşınız : " + sonuc);
+
+        }
+    }
+}
